ea0bd4d70b67d05191b6eab06aa60d52129c11ea
admin <stivrina.maria@gmail.com>
2019-02-01 00:36:15 +0700

    Добавлены файлы макета сайта + написано несколько функций, осуществляющих связи между страницами проекта

--- myserver/myserver/urls.py
+++ myserver/myserver/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from main.views import (
+    index, catalog, contacts
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('catalog/', catalog),
+    path('contacts/', contacts),
 ]
