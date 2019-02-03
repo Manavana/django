bf37c2d4cb8be310b46bae9224192bef442182dd
Manavana <stivrina.maria@gmail.com>
2019-02-04 01:06:14 +0700

    Внесены коррективы в код в связи с изменением директории страничек

--- main/views.py
+++ main/views.py
@@ -4,17 +4,11 @@ from django.shortcuts import render
 def index(request):
     return render(
         request,
-        'index.html'
-    )
-
-def catalog(request):
-    return render(
-        request,
-        'catalog.html'
+        'main/index.html'
     )
 
 def contacts(request):
     return render(
         request,
-        'contacts.html'
+        'main/contacts.html'
     )
