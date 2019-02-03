4226ef04cb8450d1a0717a36cd74eb50fd578e35
Manavana <stivrina.maria@gmail.com>
2019-02-04 01:13:03 +0700

    Сопутствующие файлы приложения products

--- myserver/urls.py
+++ myserver/urls.py
@@ -14,15 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from main.views import (
-    index, catalog, contacts
-)
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('catalog/', catalog),
-    path('contacts/', contacts),
+    path('', include('main.urls')),
+    path('', include('products.urls')),
 ]
