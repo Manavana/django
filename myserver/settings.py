af6c31edef5baa7c5316822f058409d568b47c8e
Manavana <stivrina.maria@gmail.com>
2019-02-05 02:01:51 +0700

    Добавлена директория хранения общих файлов, таких как мастер-файл

--- myserver/settings.py
+++ myserver/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'myserver.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'myserver', 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
