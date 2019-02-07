72c7738d54e2d4e269978e4ad215793d5ec07db7
Manavana <stivrina.maria@gmail.com>
2019-02-08 00:39:06 +0700

    Дописаны функции, осуществляющие чтение из json-файла с данными, а также написана функция вывода подробного описания товара

--- products/views.py
+++ products/views.py
@@ -1,8 +1,23 @@
+import json
 from django.shortcuts import render
 
 # Create your views here.
 def products(request):
-    return render(
-        request,
-        'products/products.html'
-    )
+    with open('products/fixtures/data/data.json') as file:
+        data = json.load(file)
+
+        return render(
+            request,
+            'products/products.html',
+            {'object_list': data}
+        )
+
+def product_detail_view(request, idx):
+    with open('products/fixtures/data/data.json') as file:
+        data = json.load(file)
+
+        return render(
+            request,
+            'products/detail.html',
+            {'object': data[idx]}
+        )
