9b90337855be0933642b311f7d29e2619b1328c8
Manavana <stivrina.maria@gmail.com>
2019-02-11 05:00:51 +0700

    .

--- products/views.py
+++ products/views.py
@@ -1,23 +1,28 @@
 import json
 from django.shortcuts import render
+from .models import product
 
 # Create your views here.
 def products(request):
-    with open('products/fixtures/data/data.json') as file:
-        data = json.load(file)
+    # with open('products/fixtures/data/data.json') as file:
+    #     data = json.load(file)
 
-        return render(
-            request,
-            'products/products.html',
-            {'object_list': data}
-        )
+    data = product.objects.all()
 
-def product_detail_view(request, idx):
-    with open('products/fixtures/data/data.json') as file:
-        data = json.load(file)
+    return render(
+        request,
+        'products/products.html',
+        {'object_list': data}
+    )
 
-        return render(
-            request,
-            'products/detail.html',
-            {'object': data[idx]}
-        )
+def product_detail_view(request, pk):
+    # with open('products/fixtures/data/data.json') as file:
+    #     data = json.load(file)
+
+    data = product.objects.get(pk=pk)
+
+    return render(
+        request,
+        'products/detail.html',
+        {'object': data}
+    )
