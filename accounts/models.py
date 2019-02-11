a81316885dd868b80129ae900b5a4c34874f5ceb
Manavana <stivrina.maria@gmail.com>
2019-02-11 22:51:09 +0700

    Создана модель AccountUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+class accountUser(AbstractUser):
+    avatar = models.ImageField(
+        upload_to='accounts',
+        blank=True,
+        null=True,
+    )
 
+    phone = models.CharField(
+        max_length=13,
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.username
 
 
