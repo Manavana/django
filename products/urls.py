0f966913dbc24b082127cdb0670f4b233a02274d
Manavana <stivrina.maria@gmail.com>
2019-02-12 02:36:34 +0700

    Регистрация нового класса

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from .views import (
-    products, product_detail_view, category_create_view
+    products, product_detail_view, category_create_view, category_update_view
 )
 
 urlpatterns = [
     path('', products, name='list'),
     path('<int:pk>/', product_detail_view, name='detail'),
     path('categories/create/', category_create_view, name='create'),
+    path('categories/<int:pk>/update/', category_update_view, name='update'),
 ]
