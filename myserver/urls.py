94571d90b990ae98b5c8fc0d868addd794e0fd44
Manavana <stivrina.maria@gmail.com>
2019-02-16 21:45:08 +0700

    Обновлена ссылка на url-диспетчер

--- myserver/urls.py
+++ myserver/urls.py
@@ -21,9 +21,10 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('products/', include('products.urls')),
+    path('products/', include('products.urls.products')),
     path('categories/', include('products.urls.categories')),
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
