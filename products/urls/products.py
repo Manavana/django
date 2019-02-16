e269d6a4fb00e80bc440b42cfa087527bd44727b
Manavana <stivrina.maria@gmail.com>
2019-02-16 22:16:39 +0700

    Добавлены ссылки на шаблоны create и update в url-диспетчер

--- products/urls/products.py
+++ products/urls/products.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from products.views import (
-    products, product_detail_view
+    products, product_detail_view, product_create_view, product_update_view
 )
 
 app_name = 'products'
@@ -9,4 +9,6 @@ app_name = 'products'
 urlpatterns = [
     path('', products, name='list'),
     path('<int:pk>/', product_detail_view, name='detail'),
+    path('create/', product_create_view, name='create'),
+    path('<int:pk>/update/', product_update_view, name='update'),
 ]
