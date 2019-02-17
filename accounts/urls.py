68adfa6097e8292e1b0d1c54f4226eff968543f3
Manavana <stivrina.maria@gmail.com>
2019-02-18 01:50:47 +0700

    Регистрация в url-диспетчере

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import login_view
+from .views import login_view, register_view
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', login_view, name='login')
+    path('', login_view, name='login'),
+    path('registration/', register_view, name='registration')
 ]
