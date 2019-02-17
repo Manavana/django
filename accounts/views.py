6e61e2951524b74ed681dd04a3ed38ead28ffc6b
Manavana <stivrina.maria@gmail.com>
2019-02-18 01:15:42 +0700

    Дописан контроллер, реализующий логику авторизации

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
 
 # Create your views here.
+def login_view(request):
+    form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        success_url = reverse('products:list')
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(
+                username=username,
+                password=password
+            )
+
+            if user and user.is_active:
+                login(request, user)
+
+                return redirect(success_url)
+
+    return render(
+        request,
+        'accounts/login.html',
+        {'form': form}
+    )
