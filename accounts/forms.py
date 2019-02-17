5a3dd05bce4e07f534da85643b4ff4db49209f8d
Manavana <stivrina.maria@gmail.com>
2019-02-18 01:50:25 +0700

    Форма регистрации пользователя

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import accountUser
 
 class LoginForm(forms.Form):
     username = forms.CharField(required=True, max_length=128)
@@ -7,3 +8,17 @@ class LoginForm(forms.Form):
         max_length=72,
         widget=forms.widgets.PasswordInput()
     )
+
+class RegisterForm(forms.ModelForm):
+    password_confirm = forms.CharField(
+        required=True,
+        max_length=72,
+        widget=forms.widgets.PasswordInput()
+    )
+
+    def clear_password_confirm(self):
+        pass
+
+    class Meta:
+        model = accountUser
+        fields = ['username', 'password']
