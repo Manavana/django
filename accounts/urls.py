5e3025ecc6e8b6669c4e5ae57707b47e28d7766c
Manavana <stivrina.maria@gmail.com>
2019-02-19 01:06:38 +0700

    .

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import login_view, register_view
+from django.contrib.auth.views import LoginView
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', login_view, name='login'),
+    path('', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('registration/', register_view, name='registration')
 ]
--- myserver/settings.py
+++ myserver/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'accounts.accountUser'
 
+LOGIN_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
