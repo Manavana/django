9eb48002f3b31111a2c993bffb1f09a2d6de817d
Manavana <stivrina.maria@gmail.com>
2019-02-19 00:26:31 +0700

    Регистрация контроллера в локальном url-диспетчере

--- products/urls/products.py
+++ products/urls/products.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from products.views import (
-    products, product_detail_view, product_create_view, product_update_view, product_delete_view
+    products, product_detail_view, product_create_view, product_update_view, product_delete_view, ProductListView
 )
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', products, name='list'),
+    path('', ProductListView.as_view(), name='list'),
     path('<int:pk>/', product_detail_view, name='detail'),
     path('create/', product_create_view, name='create'),
     path('<int:pk>/update/', product_update_view, name='update'),
