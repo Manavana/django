ffd322cc35d4a93d0c1d6a4f1ad3080eaba3119e
Manavana <stivrina.maria@gmail.com>
2019-02-24 20:50:31 +0700

    .

--- products/routes/products.py
+++ products/routes/products.py
@@ -1,16 +1,11 @@
 from django.urls import path
 
 from products.views import (
-    products, product_detail_view, product_create_view, product_update_view, product_delete_view, ProductListView,
-    ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
+    RestProductListView
 )
 
-app_name = 'products'
+app_name = 'rest_products'
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='list'),
-    path('<int:pk>/', ProductDetailView.as_view(), name='detail'),
-    path('create/', ProductCreateView.as_view(), name='create'),
-    path('<int:pk>/update/', ProductUpdateView.as_view(), name='update'),
-    path('<int:pk>/delete/', ProductDeleteView.as_view(), name='delete'),
+    path('', RestProductListView.as_view(), name='list'),
 ]
