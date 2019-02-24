4d6176bbf7e090b37efb771a198c92f5570bebf4
Manavana <stivrina.maria@gmail.com>
2019-02-25 01:31:59 +0700

    Регистрация rest-контроллера в локальном диспатчере

--- products/routes/categories.py
+++ products/routes/categories.py
@@ -1,16 +1,11 @@
 from django.urls import path
 
 from products.views import (
-    category_create_view, category_update_view, CategoryListView,
-    CategoryDetailView, CategoryCreateView, CategoryUpdateView, CategoryDeleteView
+    RestCategoryListView
 )
 
-app_name = 'categories'
+app_name = 'rest_categories'
 
 urlpatterns = [
-    path('create/', CategoryCreateView.as_view(), name='create'),
-    path('<int:pk>/update/', CategoryUpdateView.as_view(), name='update'),
-    path('<int:pk>/delete/', CategoryDeleteView.as_view(), name='delete'),
-    path('<int:pk>/', CategoryDetailView.as_view(), name='detail'),
-    path('', CategoryListView.as_view(), name='list'),
+    path('', RestCategoryListView.as_view(), name='list'),
 ]
