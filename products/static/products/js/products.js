8882dbb9eda58421506326986868a34a80b4790a
Manavana <stivrina.maria@gmail.com>
2019-02-24 21:56:58 +0700

    .

---
+++ products/static/products/js/products.js
@@ -0,0 +1,19 @@
+const = renserProduct = ({id, name, image, price}) => (
+    `
+    <div class="product productNew">
+        <a href="/products/${ id }" class="product_link">
+            <img src="${ image ? image : '/static/products/images/empty.png' }" alt="${ name }">
+        </a>
+        <div class="productInfo">
+            <p class="vendorCode">
+                ${ name|upper }
+            </p>
+            <div>
+                <p class="price">
+                    ${ price ? price : 'No value' } &#8381;
+                </p>
+            </div>
+        </div>
+    </div>
+    `
+)
