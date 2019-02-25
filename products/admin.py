cc5fe984ae08e14c731aa119cfd7a7e54861529b
Manavana <stivrina.maria@gmail.com>
2019-02-26 02:26:07 +0700

    Настроена админ панель приложения products

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,67 @@
 from django.contrib import admin
 from .models import product, category
+from django.template.loader import render_to_string
 
-# Register your models here.
-admin.site.register(product)
-admin.site.register(category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name', 'picture', 'category', 'price', 'modified', 'created', 'is_new'
+    ]
+
+    list_filter = [
+        'category', 'modified', 'created',
+    ]
+
+    search_fields = [
+        'name', 'description'
+    ]
+
+    fieldsets = (
+        (
+            None, {
+                'fields': ('name', 'category')
+            },
+        ),
+        (
+            'Content', {
+                'fields': ('image', 'description', 'price')
+            },
+        ),
+    )
+
+    def picture(self, obj):
+        return render_to_string(
+            'products/components/picture.html',
+            {
+                'image': obj.image
+            }
+        )
+
+
+    def is_new(self, obj):
+        return obj.modified == obj.created
+
+
+class ProductInline(admin.TabularInline):
+    model = product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'name', 'modified', 'created'
+    ]
+
+    list_filter = [
+        'modified', 'created',
+    ]
+
+    search_fields = [
+        'name', 'description'
+    ]
+
+    inlines = [
+        ProductInline
+    ]
+
+
+admin.site.register(product, ProductAdmin)
+admin.site.register(category, CategoryAdmin)
