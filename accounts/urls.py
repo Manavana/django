d4251737d3d377f0cf14a02778fbefb6f008a4c6
Manavana <stivrina.maria@gmail.com>
2019-03-01 00:42:22 +0700

    Исправлены косяки с импортом

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import login_view, register_view, AccountRegistrationView, AccountLoginView
+from .views import login_view, register_view, AccountRegistrationView, AccountLoginView, AccountLogoutView
 from django.contrib.auth.views import LogoutView
 
 app_name = 'accounts'
@@ -7,5 +7,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('', AccountLoginView.as_view(), name='login'),
     path('registration/', AccountRegistrationView.as_view(), name='registration'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', AccountLogoutView.as_view(), name='logout')
 ]
