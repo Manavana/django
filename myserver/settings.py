511a36be2c415424fc92a010ba7fbf72c593f116
Manavana <stivrina.maria@gmail.com>
2019-03-01 00:25:41 +0700

    Logout

--- myserver/settings.py
+++ myserver/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'accounts.accountUser'
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
